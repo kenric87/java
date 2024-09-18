b056177fc36cbc49bb03dc35e06657a77a44ca62
kenric87 <kenricwa@gmail.com>
2024-09-18 15:49:17 +0800

    新增資料夾，並改變其路徑

---
+++ lesson0/src/practice/ch8.java
@@ -0,0 +1,19 @@
+package practice;
+class IcCard{
+    long jj;
+    int jjj;
+
+
+}
+
+public class ch8 {
+    public static void main(String[] args) {
+        IcCard myCard;
+        myCard = new IcCard();
+        myCard.jj = 0000;
+
+    }
+    
+
+    
+}
---
+++ practice/ch8/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("挖屋，好酷喔");
+    }
+}
