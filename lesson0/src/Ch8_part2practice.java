7f6d2108e02a21cd9a1cd7c923e4a0838fd4c829
kenric87 <kenricwa@gmail.com>
2024-09-30 13:51:16 +0800

    新增練習檔案

---
+++ lesson0/src/Ch8_part2practice.java
@@ -0,0 +1,19 @@
+// 先複習
+class Iccard {
+    long id;
+    int money;
+
+    void showInfo(){
+        System.out.println("ID = "+id);
+        System.out.println("money = "+money);        
+    }
+    void addMoney(int money){
+        this.money += money;
+        System.out.println("加值" + money + "元成功");
+
+    }
+    
+}
+public class Ch8_part2practice {
+    
+}
