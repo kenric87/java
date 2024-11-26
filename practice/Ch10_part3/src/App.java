6a77294c4342edc78b88d3a94158b48f5e32aff7
kenric87 <kenricwa@gmail.com>
2024-11-26 22:23:08 +0800

    0

--- practice/Ch10_part3/src/App.java
+++ practice/Ch10_part3/src/App.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         String str;
         boolean isPWFormatted;
         boolean isDigit;
         boolean isLowerCase;
         boolean isUpperCase;
         boolean isSpecialChar;
+        
         do{
             isPWFormatted = false;
             isDigit = false;
@@ -17,14 +18,25 @@ public class App {
 
             System.out.print("請輸入密碼: ");
             str = sc.nextLine();
-
-            for (int i = 0; i <= str.length(); i++){
+            
+            for (int i = 0; i < str.length(); i++) {
                 char ch = str.charAt(i);
-                if(!isDigit && ((ch >= '0') && (ch <= '9'))){
+                if (!isDigit && (ch >= '0' && ch <= '9')) {
                     isDigit = true;
                 }
+                else if(!isLowerCase && (ch>='a'&& ch<='z')){
+                    isLowerCase=true;
+                }
+                else if (!isUpperCase && (ch>='A'&& ch<='Z'))
+                isUpperCase = true;                
             }
-        }while(!isPWFormatted);
+            isSpecialChar = str.contains("@") || str.contains("#") || str.contains("$") || str.contains("%");
 
+            isPWFormatted=isDigit&&isLowerCase&&isUpperCase&&isSpecialChar;
+            if(!isPWFormatted)
+            System.out.println("密碼格式應含大寫、小寫、數字和特殊符號");
+            else
+            System.out.println("密碼格式正確");
+        }while(!isPWFormatted);
     }
 }
