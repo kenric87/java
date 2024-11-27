1b3a9b86ce9b50800aca865c9002eecb15ed3537
kenric87 <kenricwa@gmail.com>
2024-11-27 22:48:33 +0800

    新增一些練習

--- practice/Ch11_part3/src/App.java
+++ practice/Ch11_part3/src/App.java
@@ -1,4 +1,22 @@
 //子類別只能變鬆
+class Parent1 {//父類別
+    public void Show(){
+        System.out.println("我是父類別");
+    }
+}
+
+class Child1 extends Parent1{
+    //子類別
+    //overriding 下 private 變嚴格 這樣是不行的
+    // private void Show(){
+    //     System.out.println("我是子類別");
+    // }
+}
+
+//複習 嚴格程度
+class T4 {
+    // private 
+}
 
 public class App {
     public static void main(String[] args) throws Exception {
