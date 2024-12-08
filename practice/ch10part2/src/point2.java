f6a2137b35252f66a58f5fe5fd5572e14d875229
kenric87 <kenricwa@gmail.com>
2024-12-08 23:49:42 +0800

    0

--- practice/ch10part2/src/point2.java
+++ practice/ch10part2/src/point2.java
@@ -11,7 +11,7 @@ public class point2 {
             System.out.println("請輸入整數: ");
             str = sc.next();
 
-            if(!str.matches("[0-9]+")){
+            if(!str.matches("a[0-9a-zA-Z]a")){
                 isInteger = false;
                 System.out.println("WRONG!");
             }
@@ -23,5 +23,16 @@ public class point2 {
     
 }
 /*
- * "[0-9]+"  多次整數
+ * "[0-9]+"     多次整數
+ * "hello"      單純某一個單字的，要全對
+ * "ab*a"       *前面的要不要出現，出現幾次都可以，應該是屬於最寬鬆的一種了
+ * "ab?a"       ?前的那個字可以出現0次、1次，不能超過一次
+ * "ab+a"       +前面的那個字至少要出現一次，我覺得蠻好用的，在密碼形式規範下蠻有用的
+ * "ab{2}a"     剛好{n}次
+ * 
+ * "a[bjl]a"    []內"其一"元素，出現一次
+ * "a[0-9]a"    範圍，但也是一樣，只出現一次
+ * "a[0-9][0-9]a"       可連續使用
+ * "a[a-z][0-9]a"       也有英文須注意大小寫
+ * "a[0-9a-zA-Z]a"      
  */
